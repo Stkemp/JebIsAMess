e1806aa91b76ed06fa71752cad46331b64a4c9dc
Stephen Kemp <stkemp@ucsc.edu>
2019-10-04 14:55:15 -0700

    finished assignment 1 through part 4

--- UCSC_Schoolwork/ece251/assignment1/kemp_01_v1.m
+++ UCSC_Schoolwork/ece251/assignment1/kemp_01_v1.m
@@ -11,16 +11,19 @@ clc
 
 %% 1. Determine the variance of the noise to correspond to thermal reciever
 % noise assuming temperature T = 300K and a noise figure NF = 5dB.
-Tk = 300;
-k = 1.381E-23;
-var = Tk*k/4; %see explanation of part 1
+Tk = 300; %Kelvin
+k = 1.381E-23; %Joules/Kelvin
+var = Tk*k/4; %Joules (see explanation of part 1 for formula rationale)
+fprintf('1) variance of noise: ');
+fprintf(num2str(var));
+fprintf(' Joules\n')
 
 %% 2. Design LPF and plot its mag-squared frequency response (use fir1)
 %KNOBS
 fs = 40000; % sampling frequency
 cutoff = 10000;
 order = 30;
-N = 1; % number of seconds
+N = 5; % number of seconds
 DC_gain_scalar = 2.73; % tuned to give passband gain of ~20dB
 
 Ts = 1/fs;
@@ -28,16 +31,17 @@ t = 0:Ts:N;
 
 % filt = fir1(order, cutoff/fs/2); % LPF. 1 in 2nd arg corresponds to fs/2
 filt = DC_gain_scalar.*fir1(order, 2*cutoff/fs);
-
 F_mag = abs(fft(filt, round(length(t)/2))); %frequency domain filter magnitude
 F_mag = (F_mag(1:round(length(F_mag)/2))); %just want the positive freqs 0 to pi
 F_magsqDB = 20*log(F_mag); %calculate the magnitude squared values in DB
 f = linspace(1,fs/2,length(F_magsqDB));
 
+% Plot results
 figure(1)
+subplot(3,1,1)
 plot(f,F_magsqDB)
-title('Magnitude squared LPF in frequency domain')
-xlabel('frequency (10^4 Hz)')
+title('Magnitude squared plot of LPF')
+xlabel('frequency (Hz)')
 ylabel('dB')
 
 %% 3. Generate complex noise vector and pass it through the filter to get y
@@ -47,11 +51,66 @@ r = randn(length(t),1)*std; %generate real white noise
 im = randn(length(t),1)*std; %generate imaginary white noise
 v = r + i.*im;
 
+% compute PSD of unfiltered noise
+P_v = PSD(v,t,50);
+w_v = linspace(0,fs/2,length(P_v));
+
+subplot(3,1,2)
+plot(w_v,10*log(P_v))
+title('Power Spectral Density of Unfiltered White Noise')
+xlabel('frequency (Hz)')
+ylabel('dB')
 %compute y[n] using filter
 y = filter(filt,1,v);
 %% 4. Compute and plot the spectrum of the noise at the output of the 
 % filter by averaging the magnitude squared Fourier transform of 50 blocks
 % of the output data y[n]. The frequency resolution of the spectrum should
 % be 10Hz or less.
+%KNOBS
+NF_dB = 5; %noise figure of reciever
+num_chunks = 50; %number of PSD chunks to average
+
+P = PSD(y,t,num_chunks);
+P = P.*10^(NF_dB/20); %multiply in noise figure
+w = linspace(0,fs/2,length(P));
+
+subplot(3,1,3)
+plot(w,10*log(P))
+title('Power Spectral Density of Filtered White Noise')
+xlabel('frequency (Hz)')
+ylabel('dB')
 
 %% 5. Compute the total noise power at the filter output
+
+
+
+
+
+%%--------------------------Helper functions------------------------------
+%PSD: calculates the power spectral density of a signal x using
+%Wiener-Kinchin method
+%args: x - input signal vector
+%   t - corresponding time vector
+%   N - number of chunks
+%output: P - PSD vector
+function [P] = PSD(x,t,N)
+T0 = (t(end) - t(1))/N;
+chunk_len = fix(length(x)/N);
+sum = zeros(chunk_len/2,1);
+for k = 0:N-1
+    chunk = x(chunk_len*k+1:chunk_len*(k+1));
+    Chunk = fft(chunk);
+    Chunk = Chunk(1:fix(length(Chunk)/2));
+    Chunk_magsq = abs(Chunk).^2;
+    sum = sum + Chunk_magsq./T0;
+end
+P = sum/N;
+end
+
+
+
+
+
+
+
+
